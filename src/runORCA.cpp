409fa84b379f551ffba57c61b2bf36492dbbf4c2
damanfb <daman.bareiss@utah.edu>
2013-02-06 19:01:36 -0700

    Created a test function. File is compiling but have not run. Need to add some plotting functionality

--- src/runORCA.cpp
+++ src/runORCA.cpp
@@ -13,14 +13,15 @@
     3:05pm
 */
 
+#include "runORCA.h"
+/*
 #include "Vector3.h" //     arl_RVO2_3D/Vector3.h
-#include "Agent.cpp"
+#include "Agent.h"
 #include <cstddef>
-
+*/
 using namespace RVO;
         
 /* Search for the best new velocity. */
-
 void runORCA(const Vector3& pos_rel, const Vector3& vel_a, const Vector3& vel_b, const Vector3& cmd_vel, const float& max_vel, Vector3& new_vel)
 {
     const float timeStep = 0.02;    // Time step of simulation
